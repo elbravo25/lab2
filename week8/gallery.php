2cde1609cc3779f874be48e0f12f27e10532ca76
Elijah <90529125+elbravo25@users.noreply.github.com>
2024-05-10 11:10:49 +0800

    Create gallery.php

---
+++ week8/gallery.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="style.css">
+
+    <script>
+        function newDoc() {
+            window.location.assign("https://www.w3schools.com");
+        }
+    </script>
+</head>
+<body>
+
+    <h1>Personal Profile</h1>
+
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h3>Gallery</h3>
+        <p>To be added</p>
+
+        <input type="button" value="Load new document" onclick="newDoc()">
+    </main>
+
+</body>
+</html>
